11491cfb3912416bd162e05dd854b490c58e6986
c0b2313978 <c0b2313978@edu.teu.ac.jp>
2024-11-26 13:59:30 +0900

    練習問題1: 移動量辞書を定義し、冗長な記述を改めた

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,9 +4,8 @@ import pygame as pg
 
 
 WIDTH, HEIGHT = 1100, 650
+DELTA = {pg.K_UP: (0, -5), pg.K_DOWN: (0, 5), pg.K_LEFT: (-5, 0), pg.K_RIGHT: (5, 0)}
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
-# DELTA = {pg.K_UP: (0, -5), pg.K_DOWN: (0, 5), pg.K_LEFT: (-5, 0), pg.K_RIGHT: (5, 0)}
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -25,17 +24,10 @@ def main():
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        # for key in DELTA:
-        #     if key_lst[key]:
-        #         sum_mv += DELTA[key]
-        if key_lst[pg.K_UP]:
-            sum_mv[1] -= 5
-        if key_lst[pg.K_DOWN]:
-            sum_mv[1] += 5
-        if key_lst[pg.K_LEFT]:
-            sum_mv[0] -= 5
-        if key_lst[pg.K_RIGHT]:
-            sum_mv[0] += 5
+        for pressing_key in DELTA:
+            if key_lst[pressing_key]:
+                sum_mv[0] += DELTA[pressing_key][0]
+                sum_mv[1] += DELTA[pressing_key][1]
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
         pg.display.update()
