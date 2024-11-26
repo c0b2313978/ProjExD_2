53d3d2b2e1d78e6a749e5fe956d2005313c80550
c0b2313978 <c0b2313978@edu.teu.ac.jp>
2024-11-26 14:26:15 +0900

    練習問題2: 爆弾の生成

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,4 +1,5 @@
 import os
+import random
 import sys
 import pygame as pg
 
@@ -10,10 +11,19 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
-    bg_img = pg.image.load("fig/pg_bg.jpg")    
+    bg_img = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+
+    bb_img = pg.Surface((20, 20))
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
+    bb_img.set_colorkey((0, 0, 0))
+    bb_rct = bb_img.get_rect()
+    bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    vx, vy = 5, 5
+
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -30,6 +40,14 @@ def main():
                 sum_mv[1] += DELTA[pressing_key][1]
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+
+        # if bb_rct.centerx < 0 or bb_rct.centerx > WIDTH:
+        #     vx *= -1
+        # if bb_rct.centery < 0 or bb_rct.centerx > HEIGHT:
+        #     vy *= -1
+        bb_rct.move_ip(vx, vy)
+        screen.blit(bb_img, bb_rct)
+
         pg.display.update()
         tmr += 1
         clock.tick(50)
